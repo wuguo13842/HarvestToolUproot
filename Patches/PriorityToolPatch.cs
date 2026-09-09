41da06f1ba0b05ad2da288b5c57d12591cdc23fb
wuguo13842 <106533733+wuguo13842@users.noreply.github.com>
2026-09-09 22:37:31 +0800

    错误的空间命名

--- Patches/PriorityToolPatch.cs
+++ Patches/PriorityToolPatch.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 
-namespace YourModName.Patches
+namespace AgriHarvestPriority.Patches
 {
     [HarmonyPatch(typeof(PrioritizeTool))]
     public static class PrioritizeToolPatch
